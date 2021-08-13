ce976fa9971d86d3e3e5500bcb7b4447143c12b2
marthebrevig <88542357+marthebrevig@users.noreply.github.com>
2021-08-13 13:06:07 +0200

    removed terraform version from main

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -1,22 +1,4 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
-
-  backend "remote" {
-      organization = "learnterraformss"
-      workspaces {
-        name = "Example-Workspace"
-      }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "azurerm" {
   features {}
 }
