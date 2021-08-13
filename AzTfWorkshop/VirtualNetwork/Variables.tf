fe539d08bd36853fce488ae03d0552c1804c8e65
marthebrevig <88542357+marthebrevig@users.noreply.github.com>
2021-08-13 12:41:51 +0200

    added validation to resource_group_location

--- AzTfWorkshop/VirtualNetwork/Variables.tf
+++ AzTfWorkshop/VirtualNetwork/Variables.tf
@@ -1,11 +1,19 @@
 variable "virtual_network_name" {
     description = "Name of virtual network"
     type = string
+    validation {
+        condition = length(var.virtual_network_name)>8
+        error_message = "The length of the virtual network name has to be longer than 8."
+    }
 }
 
 variable "virtual_network_adress_space" {
     description = "Adress space of virtual network"
     type = string
+    validation {
+        condition = length(var.virtual_network_adress_space)>8
+        error_message = "The length of the virtual network adress space has to be longer than 8."
+    }
 }
 
 #variable "subnet_name" {
