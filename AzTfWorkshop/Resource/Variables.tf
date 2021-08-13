4ae276f1c673eda44ee18c833cbeaee418c3a120
marthebrevig <88542357+marthebrevig@users.noreply.github.com>
2021-08-13 12:35:54 +0200

    added validation to resource_group_location

--- AzTfWorkshop/Resource/Variables.tf
+++ AzTfWorkshop/Resource/Variables.tf
@@ -12,4 +12,8 @@ variable "resource_group_location" {
     description = "Location for the resource group"
     type = string 
     default = "westeurope"
+    validation {
+        condition = length(var.resource_group_location)>8
+        error_message = "The length of the resource group location has to be longer than 8."
+    }
 }
