46ac9fa4f5583fbdf3cad6f2deb75bb824b7e7e3
marthebrevig <88542357+marthebrevig@users.noreply.github.com>
2021-08-13 16:04:28 +0200

    add network

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -3,11 +3,40 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = "my_virtual_network"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
     Name = var.instance_name
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
   }
 }
