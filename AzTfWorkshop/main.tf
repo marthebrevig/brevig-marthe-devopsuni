d474d50996dcab3f5822c5a3ecf09e5ead1881bb
marthebrevig <88542357+marthebrevig@users.noreply.github.com>
2021-08-13 12:15:14 +0200

    add more .tf-files

---
+++ AzTfWorkshop/main.tf
@@ -0,0 +1,23 @@
+module "my_first_module" {
+    source = ".//Resource"
+}
+
+module "my_second_module" {
+    source = ".//VirtualNetwork"
+    virtual_network_name = "testingvn"
+    virtual_network_adress_space = "10.0.0.0/16"
+    subnets = {
+        "subnet1" = {
+            name = "testing1"
+            address_space = ["10.0.0.0/24"]
+        }
+        "subnet2" = {
+            name = "testing2"
+            address_space = ["10.0.1.0/24"]
+        }
+        "subnet3" = {
+            name = "testing3"
+            address_space = ["10.0.2.0/24"]
+        }
+    }
+}
